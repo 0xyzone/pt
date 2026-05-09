47bec525a664b2828cedb97136c64c55a531c48f
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-05-09 17:09:06 +0545

    Update postmatch.blade.php

--- resources/views/screens/postmatch.blade.php
+++ resources/views/screens/postmatch.blade.php
@@ -16,10 +16,7 @@
         </thead>
         <tbody>
             @foreach ($activeMatch->matchStats->sortByDesc(['points']) as $match)
-            @php
-            $alive = $match->alive;
-            @endphp
-            <tr class="text-center {{ $alive === 0 ? 'bg-red-300' : 'bg-white' }}">
+            <tr class="text-center">
                 <td class="uppercase pr-3 font-bold border border-neutral-300 py-1.5 relative overflow-hidden">
                     @if ($match->tournamentTeam->logo_image)
                     <img src="{{ asset('storage/' . $match->tournamentTeam->logo_image) }}" alt="{{ $activeMatch->tournamentTeam?->name . ' logo' }}" class="w-32 rotate-4 -top-10 -left-10 object-cover absolute mask-r-from-20% mask-r-to-80%">
