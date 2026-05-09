ef9207cd2a08580e59cf29608bede202ccf4d16f
0xyzone <58982560+0xyzone@users.noreply.github.com>
2026-05-09 16:52:24 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/css/filament/maidan/theme.css'],
+            input: [
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/css/filament/maidan/theme.css",
+            ],
             refresh: true,
         }),
         tailwindcss(),
     ],
     server: {
         watch: {
-            ignored: ['**/storage/framework/views/**'],
+            ignored: ["**/storage/framework/views/**"],
         },
+    },
+    build: {
+        // Attempting to force a more stable build path
+        minify: "terser", // Switching from 'esbuild' to 'terser' sometimes bypasses the panic
     },
 });
